6f757c42cdae3254acf8091f79a96c0e06b95879
roshanverma2001 <roshanverma01@gmail.com>
2021-12-11 21:58:29 -0600

    added test data

--- convert_pics_to_vid.py
+++ convert_pics_to_vid.py
@@ -1,22 +1,61 @@
 import cv2
 import os
 
-image_folder = 'VisDrone2019-MOT-train/sequences/uav0000013_00000_v'
-video_name = './converted_videos/uav0000013_00000_v'
-extension = ".mp4"
 
-curr_video_name = video_name + extension
 
-images = sorted([img for img in os.listdir(image_folder) if img.endswith(".jpg")])
+def convert_video(source_directory = "VisDrone2019-MOT-train/sequences/", target_directory = "./converted_videos_train/"):
+   
+    extension = ".mp4"
 
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-video = cv2.VideoWriter(curr_video_name, fourcc, 1, (width,height))
 
-for image in images:
-    video.write(cv2.imread(os.path.join(image_folder, image)))
+    count = 0
+    for filename in os.listdir(source_directory):
+        image_folder = os.path.join(source_directory, filename)
+        target_video_name = os.path.join(target_directory, filename)
+        target_video_name = target_video_name + extension
 
-cv2.destroyAllWindows()
-video.release()
+        
+        if os.path.exists(target_video_name):
+            print(target_video_name, "exists")
+            continue
+
+       # print("Folder {} has target {}".format(image_folder, target_video_name))
+        # print(image_folder)
+      
+        images = sorted([img for img in os.listdir(image_folder) if img.endswith(".jpg")])
+        frame = cv2.imread(os.path.join(image_folder, images[0]))
+        height, width, layers = frame.shape
+
+        video = cv2.VideoWriter(target_video_name, fourcc, 1, (width,height))
+
+        for image in images:
+            video.write(cv2.imread(os.path.join(image_folder, image)))
+        print("Done with : ", target_video_name)
+        count += 1
+
+        cv2.destroyAllWindows()
+        video.release()
+    
+    print("Finished writing all to videos")
+
+# image_folder = 'VisDrone2019-MOT-train/sequences/uav0000013_00000_v'
+# video_name = './converted_videos/uav0000013_00000_v'
+# extension = ".mp4"
+
+# curr_video_name = video_name + extension
+
+# images = sorted([img for img in os.listdir(image_folder) if img.endswith(".jpg")])
+
+# frame = cv2.imread(os.path.join(image_folder, images[0]))
+# height, width, layers = frame.shape
+
+# fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+# video = cv2.VideoWriter(curr_video_name, fourcc, 1, (width,height))
+
+# for image in images:
+#     video.write(cv2.imread(os.path.join(image_folder, image)))
+
+# cv2.destroyAllWindows()
+# video.release()
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import create_tracker
 import locate_objects
 import cv2
+from convert_pics_to_vid import *
+import zipfile
 
 
 def main():
-    #  convert_video()
-    tracker = create_tracker.returnTrackerName("Boosting")
-    multi_tracker = cv2.legacy.MultiTracker_create()
+    # path_to_zip_file = "VisDrone2019-MOT-test-dev.zip"
+    # directory_to_extract_to = "./VisDrone2019-MOT-test-dev"
+    # with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+    #     zip_ref.extractall(directory_to_extract_to)
+    test_vids_src = "./VisDrone2019-MOT-test-dev/VisDrone2019-MOT-test-dev/sequences"
+    test_vids_dest = "./converted_videos_test/"
+    convert_video(test_vids_src, test_vids_dest)
+    # tracker = create_tracker.returnTrackerName("Boosting")
+    # multi_tracker = cv2.legacy.MultiTracker_create()
 
-    locate_objects.define_bounding_boxes(
-        video_path="./converted_videos/uav0000013_00000_v.mp4",
-        # annotations_path="VisDrone2019-MOT-train/annotations/uav0000013_00000_v.txt",
-        annotations_path="uav0000013_00000_v.txt",
-        tracker=tracker,
-        multiTracker=multi_tracker)
+    # locate_objects.define_bounding_boxes(
+    #     video_path="./converted_videos/uav0000281_00460_v.mp4",
+    #     annotations_path="VisDrone2019-MOT-train/annotations/uav0000013_00000_v.txt",
+    #   #  annotations_path="uav0000013_00000_v.txt",
+    #     tracker=tracker,
+    #     multiTracker=multi_tracker)
 
-    # locate_objects.initialize_multiTracker(tracker=tracker,multiTracker=multi_tracker,boundingBoxes=bounding_boxes, frame = frame)
+   # locate_objects.initialize_multiTracker(tracker=tracker,multiTracker=multi_tracker,boundingBoxes=bounding_boxes, frame = frame)
 
 
 if __name__ == "__main__":
